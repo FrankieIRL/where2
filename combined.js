41e0fe74bf55662d3c76e9b1210bf82ba9087754
FrankieIRL <44464227+FrankieIRL@users.noreply.github.com>
2018-11-22 18:54:41 +0000

    Add files via upload

--- combined.js
+++ combined.js
@@ -7,7 +7,7 @@ let fs = require('fs');
 let clubs = ['pygmalion', 'diceys', 'coppers', 'house', 'opium rooms', 'krystle', 'dtwo'];
 let dates = ['JT Pimms', 'pygmalion', 'the jar'];
 let pints = ['toners', 'the living room', 'whelans'];
-let sample = ['sams bar'];
+let sample = ['sams bar', 'toners'];
 
 let venueType = [ ];
 
@@ -29,7 +29,7 @@ let optionsMobile = {
 	},
 	shotSize: {
 		width: 414,
-		height: 2100
+		height: 736
 	},
 	userAgent: 'Mozilla/5.0 (iPhone9,4; U; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/14A403 Safari/602.1'
 };
@@ -38,33 +38,55 @@ let optionsMobile = {
 // It works now IF there is only one entry in the sample array. If there are two it does not. 
 // Note that the webshot uses venueType[i], whereas this just uses venueType. venueType[i] breaks this
 function ts() {
-	Tesseract.recognize(venueType+'.png')
-  .then(function (result) {fs.writeFileSync(venueType+'.txt', result.text, function(err) {
+		Tesseract.recognize(venueType[i]+'.png')
+  .then(function (result) {fs.writeFileSync(venueType[i]+'.txt', result.text, function(err) {
 		if (err) {
 		    console.log(err);
 		} else {
 		    console.log("file written successfully"); // it does not console.log this for some reason
 			}
 		})
-    process.exit(0) // if this is removed the command prompt runs forever, it doesn't stop
+  		if (venueType.length === i) {
+    process.exit(0)  			
+  		}
+//    process.exit(0) // if this is removed the command prompt runs forever, it doesn't stop. 
+//If it stays in, it stops after the first (sams bar)
   })
 };
 
-//this for loop runs the webshot module for every value in the user's chosen venue type. 
-// It then runs the ts function
-// It works with just one value in the sample array
-// There is probably  better way to do this.
 
-var i;
-for (i = 0; i < venueType.length; i++) { 
-webshot('https://www.google.ie/search?q='+venueType[i]+'+dublin', 
+function ws () {
+	webshot('https://www.google.ie/search?q='+venueType[i]+'+dublin', 
 	venueType[i]+'.png', optionsMobile, function(err) { 
 		if (!err) {
 			console.log('Screenshot taken!');
-			ts(venueType[i]);
+			ts();
 		}
 	})
 };
+
+
+for (let i = 0; i < venueType.length; i++) {
+	webshot('https://www.google.ie/search?q='+venueType[i]+'+dublin', 
+	venueType[i]+'.png', optionsMobile, function(err) { 
+		if (!err) {
+			console.log('Screenshot taken!');
+	Tesseract.recognize(venueType[i]+'.png')
+    .then(function (result) {fs.writeFileSync(venueType[i]+'.txt', result.text, function(err) {
+		if (err) {
+		    console.log(err);
+		} else {
+		    console.log("file written successfully"); // it does not console.log this for some reason
+			}
+		})
+//    process.exit(0) // if this is removed the command prompt runs forever, it doesn't stop. 
+//If it stays in, it stops after the first (sams bar)
+  		});
+		}
+	})
+};
+
+
 
 // this is unneccessary
 console.log(venueType.length);
