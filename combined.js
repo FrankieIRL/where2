5b35d89b636d094bcadde70e854dd8ddcfd11ca6
FrankieIRL <44464227+FrankieIRL@users.noreply.github.com>
2018-11-23 18:18:20 +0000

    Updated, flow working kind of
    More or less working. 
    Events happen in order. But if TS is used the process still doesn't end.
    Read() is reading sample data and works but I need to get it to update the array in the right place.
    Tesseract not accurate so it's parsing venue+'sample.txt'

--- combined.js
+++ combined.js
@@ -1,3 +1,10 @@
+/*
+2/25 days (~18hrs)
++ 6 hrs thur
+
+
+*/
+
 //this loads the webshot library from program files
 let webshot = require('webshot');
 let Tesseract = require('tesseract.js');
@@ -7,7 +14,10 @@ let fs = require('fs');
 let clubs = ['pygmalion', 'diceys', 'coppers', 'house', 'opium rooms', 'krystle', 'dtwo'];
 let dates = ['JT Pimms', 'pygmalion', 'the jar'];
 let pints = ['toners', 'the living room', 'whelans'];
-let sample = ['sams bar', 'toners'];
+let sample = [
+		{venueName: 'toners', status: 'none' },
+		{venueName: 'diceys', status: 'none' }
+]
 
 let venueType = [ ];
 
@@ -29,44 +39,57 @@ let optionsMobile = {
 	},
 	shotSize: {
 		width: 414,
-		height: 736
+		height: 1400
 	},
 	userAgent: 'Mozilla/5.0 (iPhone9,4; U; CPU iPhone OS 10_0_1 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) Version/10.0 Mobile/14A403 Safari/602.1'
 };
 
-// This is the tesseract function(?) that reads the file
-// It works now IF there is only one entry in the sample array. If there are two it does not. 
-// Note that the webshot uses venueType[i], whereas this just uses venueType. venueType[i] breaks this
-function ts() {
-		Tesseract.recognize(venueType[i]+'.png')
-  .then(function (result) {fs.writeFileSync(venueType[i]+'.txt', result.text, function(err) {
-		if (err) {
-		    console.log(err);
-		} else {
-		    console.log("file written successfully"); // it does not console.log this for some reason
-			}
-		})
-  		if (venueType.length === i) {
-    process.exit(0)  			
-  		}
+
+const ws = (venue) => {
+	webshot('https://www.google.ie/search?q='+venue+'+dublin', 
+	venue+'.png', optionsMobile, function(err) { 
+		if (!err) {
+			console.log(venue+' screenshot taken')
+			ts(venue)
+		}
+	})
+};
+
+
+const ts = (venue) => {
+		Tesseract.recognize(venue+'.png')
+  		.then(function (result) {fs.writeFileSync(venue+'.txt', result.text)
+  		console.log(venue+' text file created')
+  		read(venue)
 //    process.exit(0) // if this is removed the command prompt runs forever, it doesn't stop. 
 //If it stays in, it stops after the first (sams bar)
   })
 };
 
+const read = (venue) => {
+	fs.readFile(venue+'sample.txt', "utf8", function (err, data) {
+	  if (err) throw err;
+   		let alltext = data;
+   		console.log(venue+' '+(alltext.slice(alltext.search("Live:"),alltext.search("Live:")+15)));
+})
+}
 
-function ws () {
-	webshot('https://www.google.ie/search?q='+venueType[i]+'+dublin', 
-	venueType[i]+'.png', optionsMobile, function(err) { 
-		if (!err) {
-			console.log('Screenshot taken!');
-		}
-	})
+
+
+
+// this works but does not end the programme if tesseract is involved 
+for (let i = 0; i < venueType.length; i++) {
+	let venue = venueType[i].venueName; // this takes the venueName variable from the venueType array and calls it venue
+	read(venue) // it then runs venue here through whatever function
 };
+
+
+
 
 // this runs both the webshot and ocr
 // the ocr only wiorks here bc it is part of the if !err clause in the above function
 // it does not end the process. No matter where I put process.exit(0) there is a problem
+/*
 for (let i = 0; i < venueType.length; i++) {
 	webshot('https://www.google.ie/search?q='+venueType[i]+'+dublin', 
 	venueType[i]+'.png', optionsMobile, function(err) { 
@@ -86,8 +109,29 @@ for (let i = 0; i < venueType.length; i++) {
 		}
 	})
 };
-
+*/
 
 
 // this is unneccessary
 console.log(venueType.length);
+
+/*
+functions in old sytax
+function ts(venue) {
+		Tesseract.recognize(venue+'.png')
+  		.then(function (result) {fs.writeFileSync(venue+'.txt', result.text)
+//    process.exit(0) // if this is removed the command prompt runs forever, it doesn't stop. 
+//If it stays in, it stops after the first (sams bar)
+  })
+};
+
+
+function ws (venue) {
+	webshot('https://www.google.ie/search?q='+venue+'+dublin', 
+	venue+'.png', optionsMobile, function(err) { 
+		if (!err) {
+			console.log('Screenshot taken!');
+		}
+	})
+};
+*/
